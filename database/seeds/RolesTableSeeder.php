9fb3de7a07303c88df986d2cc41d92feb70387ef
paraparadox <paraparadox@protonmail.com>
2020-03-17 18:10:19 +0500

    The end of the 14th video

--- database/seeds/RolesTableSeeder.php
+++ database/seeds/RolesTableSeeder.php
@@ -12,7 +12,16 @@ class RolesTableSeeder extends Seeder
      */
     public function run()
     {
-        Role::truncate();
+        //Чтобы избежать ошибки
+        //SQLSTATE[42000]: Syntax error or access violation: 1701
+        //Cannot truncate a table referenced in a foreign key constraint
+        //(`ums_db`.`role_user`, CONSTRAINT `role_user_role_id_foreign`
+        //FOREIGN KEY (`role_id`) REFERENCES `ums_db`.`roles` (`id`))
+        //(SQL: truncate table `roles`)
+        //будем использовать метод delete() вместо truncate()
+        //---------
+        //Role::truncate();
+        DB::table('roles')->delete();
 
         Role::create(['name' => 'admin']);
         Role::create(['name' => 'author']);
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,9 +14,17 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        DB::table('role_user')->truncate();
-
+        //Чтобы избежать ошибки
+        //SQLSTATE[42000]: Syntax error or access violation: 1701
+        //Cannot truncate a table referenced in a foreign key constraint
+        //(`ums_db`.`role_user`, CONSTRAINT `role_user_user_id_foreign`
+        //FOREIGN KEY (`user_id`) REFERENCES `ums_db`.`users` (`id`))
+        //(SQL: truncate table `users`)
+        //будем использовать метод delete() вместо truncate()
+        //---------
+        //User::truncate();
+        //DB::table('role_user')->truncate();
+        DB::table('role_user')->delete();
         $adminRole = Role::where('name', 'admin')->first();
         $authorRole = Role::where('name', 'author')->first();
         $userRole = Role::where('name', 'user')->first();
